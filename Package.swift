96530a50704dba4155852cb6e5caf4eaecd263f8
Andrey Lukyantsev <al@4010.ru>
2026-07-29 13:13:48 +0300

    test: make swift testing toolchain-aware

--- Package.swift
+++ Package.swift
@@ -1,5 +1,44 @@
 // swift-tools-version: 6.0
 import PackageDescription
+import Foundation
+
+let fileManager = FileManager.default
+let developerDirectories = [
+    ProcessInfo.processInfo.environment["DEVELOPER_DIR"],
+    "/Applications/Xcode.app/Contents/Developer",
+    "/Library/Developer/CommandLineTools",
+].compactMap { $0 }
+
+let testingFrameworkDirectory = developerDirectories
+    .map { "\($0)/Library/Developer/Frameworks" }
+    .first { fileManager.fileExists(atPath: "\($0)/Testing.framework") }
+let testingInteropDirectory = developerDirectories
+    .map { "\($0)/Library/Developer/usr/lib" }
+    .first { fileManager.fileExists(atPath: "\($0)/lib_TestingInterop.dylib") }
+
+let testingSwiftSettings: [SwiftSetting] = testingFrameworkDirectory.map {
+    [.unsafeFlags([
+        "-F", $0,
+    ])]
+} ?? []
+let testingLinkerSettings: [LinkerSetting] = {
+    var settings: [LinkerSetting] = []
+    if let testingFrameworkDirectory {
+        settings.append(.unsafeFlags([
+            "-F", testingFrameworkDirectory,
+            "-Xlinker", "-rpath",
+            "-Xlinker", testingFrameworkDirectory,
+        ]))
+        settings.append(.linkedFramework("Testing"))
+    }
+    if let testingInteropDirectory {
+        settings.append(.unsafeFlags([
+            "-Xlinker", "-rpath",
+            "-Xlinker", testingInteropDirectory,
+        ]))
+    }
+    return settings
+}()
 
 let package = Package(
     name: "QuickCal",
@@ -12,20 +51,8 @@ let package = Package(
         .testTarget(
             name: "QuickCalKitTests",
             dependencies: ["QuickCalKit"],
-            swiftSettings: [
-                .unsafeFlags([
-                    "-F", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
-                ]),
-            ],
-            linkerSettings: [
-                .unsafeFlags([
-                    "-F", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
-                    "-Xlinker", "-rpath",
-                    "-Xlinker", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
-                    "-Xlinker", "-rpath",
-                    "-Xlinker", "/Library/Developer/CommandLineTools/Library/Developer/usr/lib",
-                ]),
-            ]
+            swiftSettings: testingSwiftSettings,
+            linkerSettings: testingLinkerSettings
         ),
     ]
 )
